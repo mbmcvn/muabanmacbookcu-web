790d797da7f9bbdcad39e8afde3b353f90c6a298
auryesvn <auryes.vn@gmail.com>
2026-07-23 17:43:57 +0700

    Re-direct care

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,15 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "img.mbmc.vn", pathname: "/machines/**" },
     ],
   },
+   async redirects() {
+    return [
+      {
+        source: "/care/:path*",
+        destination: "https://mbmc-care.vercel.app/care/:path*",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
